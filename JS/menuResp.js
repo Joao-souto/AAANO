2281f18d98f5e927b42b75709e45356c649facfb
Windows <joaosouto0@outlook.com>
2023-11-30 00:27:10 -0300

    Finalizando

--- JS/menuResp.js
+++ JS/menuResp.js
@@ -1,6 +1,14 @@
+
 const menuToggle = document.querySelector(".menuToggle");
 const header = document.querySelector("header");
 
-menuToggle.addEventListener("click", ()=>{
-    header.classList.toggle('active')
-})
+menuToggle.addEventListener("click", () => {
+    header.classList.toggle('active');
+});
+
+const btn2 = document.querySelector("#btn2");
+const ptRover = document.querySelector("#ptRover");
+
+btn2.addEventListener("click", () => {
+    ptRover.style.display = (ptRover.style.display === 'block') ? 'none' : 'block';
+});
